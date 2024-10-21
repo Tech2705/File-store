b301e1f1e525c7d503292ac6ff7cf78430b43db9
Tech2705 <160301845+Tech2705@users.noreply.github.com>
2024-10-21 17:23:39 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -96,7 +96,7 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/codeflix_bots")
         self.LOGGER(__name__).info(f"""""")      
- self.LOGGER(__name__).info(f""" \n\n       
+ self.LOGGER(__name__).info(f"""      
                                                    
                   
                                  
